84004353f93751f76b0c9c82bbbaf254c462e85e
atharvbhute <atharv.bhute18@gmail.com>
2023-09-18 09:14:03 +0530

    Learning date

---
+++ Basics/08_dateTime.js
@@ -0,0 +1,6 @@
+// Date object always returns a date in milliseconds from January 1, 1970 UTC (the epoch) 
+
+// refer this https://tc39.es/proposal-temporal/docs/
+
+let myDate =  new Date();
+console.log(myDate);
