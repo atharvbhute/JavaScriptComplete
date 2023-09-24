c9158d487fca37af9f56f30ff31d4906f024326c
atharvbhute <atharv.bhute18@gmail.com>
2023-09-25 00:25:18 +0530

    Learning functions

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,8 @@
+// basic function
+
+function sayHelloMe(name){
+    console.log(`Hey! ${name}`); 
+}// {} this is scope
+
+sayHelloMe("Atharv"); // only name of the function without () is the referrence of the function and with () is when you use function
+ 
