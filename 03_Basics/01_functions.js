4cac8a06f25e23820f341a24135933333d0e4867
atharvbhute <atharv.bhute18@gmail.com>
2023-09-26 00:35:17 +0530

    learning functions using array and Objects

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -36,3 +36,47 @@ function loggedUser(username = "Sam"){ // here we have given username a default 
 }
 
 console.log(loggedUser());
+
+// multiple parameters and multiple arguments
+function calculateCartPrice(...items){ //with ...parameterName, we call these three dots a rest operator.
+    return items
+}
+
+console.log(calculateCartPrice(100,200,366)); // [ 100, 200, 366 ] returned an array
+
+// another way
+function anotherCart(item1, item2, ...itemss){
+    return itemss;
+}
+
+console.log(anotherCart(10,20,30,40,50)); //[ 30, 40, 50 ] as 10 and 20 was assigned to item1 and 2 respectively and the rest values was assigned to an array
+
+// giving object as an argument
+
+let aObject = {
+    id : 1,
+    name: "Tada"
+}
+
+function myFunctionObject(mahObject){
+    return mahObject.id;
+}
+
+console.log(myFunctionObject(aObject)); // 1
+
+/* we can directly give object directly as a argument myFunctionObject({id: 1})
+the only issue is when we don't pass a function in that case we need to check type in that code and that is why people prefer typescript to avoid
+That code*/
+
+// passing an Array
+
+
+let anArray = [1,2,3,4,5,6,7,8,9];
+
+function myFunctionArray(mahArray){
+    return mahArray[1];
+}
+
+console.log(myFunctionArray(anArray)); // 2
+
+/* we can directly give an array directly as a argument myFunctionArray([1,2,3,4,5,6,7]) */
