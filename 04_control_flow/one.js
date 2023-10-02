d11871d8859c6921ab90c390f6865b128e9e2f22
atharvbhute <atharv.bhute18@gmail.com>
2023-10-03 00:29:26 +0530

    conditional flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,22 @@
+// control flow or logic flow
+//eg. if you are logged in another code will run and if you are logged out another code will learn that is control flow or logic flow
+// using if else
+
+// if (condition) { // conditions like >,<,>=,<=,==,===,!=,!==s
+    
+// } else {
+    
+// }
+
+// implicit scope
+if (condition) console.log("Test"),
+console.log("Second line"); // this is one liner scope  or implicit scope to use second line with coma and always end with ; but this is bad practice
+
+//nesting 
+// if (condition) {
+    
+// } else if (condition) {
+    
+// }else if (condition) {
+    
+// }
