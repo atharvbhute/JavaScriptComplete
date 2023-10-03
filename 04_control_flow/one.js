96b25e9b57d3df147133a14efc4752803f899fe6
atharvbhute <atharv.bhute18@gmail.com>
2023-10-03 10:23:41 +0530

    conditional flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -9,8 +9,8 @@
 // }
 
 // implicit scope
-if (condition) console.log("Test"),
-console.log("Second line"); // this is one liner scope  or implicit scope to use second line with coma and always end with ; but this is bad practice
+// if (condition) console.log("Test"),
+// console.log("Second line"); // this is one liner scope  or implicit scope to use second line with coma and always end with ; but this is bad practice
 
 //nesting 
 // if (condition) {
@@ -20,3 +20,25 @@ console.log("Second line"); // this is one liner scope  or implicit scope to use
 // }else if (condition) {
     
 // }
+
+// if (true && false || true) { // using and and or 
+    
+// }
+
+let month = 1;
+switch (month) {
+    case 1: 
+        console.log("January");
+        break;        
+
+    case 2: 
+        console.log("Feb");        
+        break;
+
+    default:
+        console.log("Default");
+        break;
+}
+// you can give strings as well
+
+
