a37f2750c403d4819fdfd9d579b6245ddf5c7e2f
atharvbhute <atharv.bhute18@gmail.com>
2023-11-19 02:25:40 +0530

    classes

---
+++ OOP/04_classes.js
@@ -0,0 +1,39 @@
+class User{
+    constructor(username, password){
+        this.username = username;
+        this.password = password;
+    }// constructor function calls whenever we call this class or create new instance;
+
+    getUserName(){
+        console.log(this.username);
+    }
+} // this is a common class syntax
+
+const userOne = new User("atharv","pass");
+userOne.getUserName();
+
+// ++++++++++++++++ without using class keyword or syntactic suger +++++++++++++++++
+
+
+function CreateUser(username, password) {
+    this.username = username;
+    this.password = password;
+}
+
+CreateUser.prototype.getUserPassword = function() {
+    return this.password+234;
+}
+
+const userTwo = new CreateUser("John","Password");
+
+
+// ++++++++++++++++++++ inheritance +++++++++++++++++++++++++++++++++
+
+class createTaskList{
+    constructor(task,id){
+        this.task = task;
+        this.id = id;
+    }
+
+    
+}
