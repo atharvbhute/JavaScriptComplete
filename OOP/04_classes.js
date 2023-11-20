0dfc853dc54c5a2ab0d7b611690aba50e716c6f4
atharvbhute <atharv.bhute18@gmail.com>
2023-11-21 01:26:41 +0530

    classes

--- OOP/04_classes.js
+++ OOP/04_classes.js
@@ -37,6 +37,44 @@ class createTaskList{
 }
  class updateTask extends createTaskList{
     constructor(task, id){
-        
+        super(task,id);        
+    }
+
+    printOne(completed){
+        console.log(`${this.task} is ${completed} with id ${this.id}`);
     }
  }
+
+ const newTask = new updateTask("Get Milk", "55");
+ newTask.printOne(true);
+
+ // ++++++++++++++++++++ Static +++++++++++++++++++++++++++++++++++++++
+ /*
+ Static methods are often utility functions, such as functions to create or clone objects, 
+ whereas static properties are useful for caches, fixed-configuration, 
+ or any other data you don't need to be replicated across instances.
+ */
+
+ class makeBeer{
+    constructor(flavor){
+        this.flavor = flavor    
+        this.alcohol = 55;
+    }
+
+    brewer(){
+        console.log(`your ${this.flavor} flavored beer is in making
+        alcohol % = ${this.alcohol}`);
+    }
+
+    static hideMe(){
+        console.log("actual alcohol % is 100%");
+    } // now this function is not available to any instance this is only available to this class invocation,
+    // static function is not even accessible within class. 
+ 
+}
+
+ const newBeer = new makeBeer("weet");
+
+newBeer.brewer();
+
+ 
